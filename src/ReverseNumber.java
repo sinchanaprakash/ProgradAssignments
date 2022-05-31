6289d599de324306d094f2c4b94f3826cc1d5111
sinchana <sinchana9816@gmail.com>
2022-05-31 17:43:31 +0530

    Reverse of a number

---
+++ src/ReverseNumber.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ReverseNumber {
+    public static void main(String args[]){
+        int num,num1,num2,num3,reverse;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Number to be reversed");
+        num=sc.nextInt();
+        {
+            num1 = num/100;
+            num2 = (num%100)/10;
+            num3 = num % 10;
+            reverse = 100*num3 + 10*num2 + num1;
+        }
+        System.out.println("reverse number is " +reverse);
+    }
+}
