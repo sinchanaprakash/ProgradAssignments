26abc640f24fd325c35ba20451365556f8acaa2d
sinchana <sinchana9816@gmail.com>
2022-05-31 21:46:16 +0530

    calculating final price with discount

---
+++ src/CalculateDiscount.java
@@ -0,0 +1,23 @@
+ import java.util.Scanner;
+
+ public class CalculateDiscount {
+
+     public static void main(String args[])
+     {
+         double price, quantity, discount, total;
+
+         Scanner sc = new Scanner(System.in);
+         System.out.println("Enter the Price and Number of quntity ");
+         price = sc.nextInt();
+         quantity = sc.nextInt();
+
+         if(quantity>=10) {
+             discount = 0.1 * price;
+             total = (price*quantity) - discount;
+         }
+         else {
+             total = price*quantity;
+         }
+         System.out.println("The price of the product :" +total);
+     }
+}
