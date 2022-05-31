695af29b9d6295e2e611c44ac1f8aa969f17c724
sinchana <sinchana9816@gmail.com>
2022-05-31 19:56:34 +0530

    Sum of First and Last

---
+++ src/FirstLast.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class FirstLast {
+    public static void main(String args[]){
+        int num1,num2,num3,num,reverse=0,first,last,sum=0;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Number");
+        num=sc.nextInt();
+        {
+            last = num % 10;
+            first = num / 1000;
+            sum = first + last;
+        }
+        System.out.println("Sum is " +sum);
+    }
+}
