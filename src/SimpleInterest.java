a4c6a0f2b1f24c2d9ae3d17b2d4b36d170f5b0b0
sinchana <sinchana9816@gmail.com>
2022-05-31 17:22:52 +0530

    SimpleInterest

---
+++ src/SimpleInterest.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class SimpleInterest {
+    public static void main(String args[]) {
+        float p,r= 7.5F,t,SI,TotalAmount;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Principal");
+        p=sc.nextFloat();
+        System.out.print("Enter the Time");
+        t=sc.nextFloat();
+        SI=(p*r*t)/100;
+        TotalAmount=p+SI;
+        System.out.print("Simple interest is " +SI);
+        System.out.print("\nFinal amount paid to the bank is " +TotalAmount);
+    }
+}
