c2029184cd12635c0f4b6cb1c0a7220aa19ecacf
sinchana <sinchana9816@gmail.com>
2022-05-31 21:29:00 +0530

    Convert days into Monthd and Days

---
+++ src/DaysToMonths.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class DaysToMonths {
+
+        public static void main(String args[]) {
+
+            int days,num,month;
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter the number of days");
+            num = sc.nextInt();
+
+            days = num % 30;
+            month = num / 30;
+            System.out.println( "Month and days = " + month + " Month and " + days + " days");
+        }
+    }
+
