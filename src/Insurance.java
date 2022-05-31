e8581afa9ab0f123b33aca20b8fa8529b93c2733
sinchana <sinchana9816@gmail.com>
2022-05-31 23:14:53 +0530

    To check if the driver is insured or not

---
+++ src/Insurance.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Insurance {
+    public static void main(String args[]) {
+        int age;
+        char ms, sex;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the age of the driver");
+        age = sc.nextInt();
+        System.out.println("Enter the marital status of the driver \n M for married and U for unmarried");
+        sex = sc.next().charAt(0);
+        System.out.println("Enter the sex of the driver \n M for Male and F for Female");
+        ms = sc.next().charAt(0);
+
+        if (ms == 'M') {
+            System.out.println("Driver is insured");
+        }
+        else if (ms == 'U' & age >= 30 && sex == 'M'){
+
+            System.out.println("Driver is insured");
+        }
+        else if (ms == 'U' && age >= 25 && sex == 'F'){
+            System.out.println("Driver is insured");
+        }
+        else{
+            System.out.println("Driver is not insured");
+        }
+
+    }
+}
